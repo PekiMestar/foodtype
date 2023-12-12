2a7839a8d9acfd50a552059ef1081b0ae1f2cc5f
PekiMestar <145533879+PekiMestar@users.noreply.github.com>
2023-12-12 11:16:31 +0100

    Add files via upload

---
+++ foodType/FoodType.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foodType
+{
+    internal class FoodType
+    {
+        public string name;
+        public int protein;
+        public int carbs;
+        public int fat;
+        static int counter;
+
+        public void setname(string name)
+        {
+            this.name = name;
+        }
+
+        public void setprotein(int protein)
+        {
+            this.protein = protein;
+        }
+
+        public void setcarbs(int carbs)
+        {
+            this.carbs = carbs;
+        }
+
+        public void setfat(int fat)
+        {
+            this.fat = fat;
+        }
+
+        public string getname()
+        {
+            return this.name;
+        }
+
+        public int getprotein()
+        {
+            return this.protein;
+        }
+
+        public int getcarbs()
+        {
+            return this.carbs;
+        }
+
+        public int getfat()
+        {
+            return this.fat;
+        }
+
+        public int getCounter()
+        {
+            return counter;
+        }
+
+        public override string ToString()
+        {
+            string ispis = name + " p - " + this.getprotein() + "%"
+            + " c - " + this.getcarbs() + "%"
+            + " f - " + this.getfat() + "%";
+
+            return ispis;
+        }
+
+        public FoodType(string name, int protein, int carbs, int fat)
+        {
+            this.name = name;
+            this.protein = protein;
+            this.carbs = carbs;
+            this.fat = fat;
+            counter++;
+        }
+
+        private FoodType() { }
+    }
+}
---
+++ foodType/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foodType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FoodType banana = new FoodType("banana", 4, 93, 3);
+            
+            Console.WriteLine(banana.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
